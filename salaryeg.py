94175bfcfd3d1da6919e9398b9d6ecea612b11c3
Anish <thapaanish793@gmail.com>
2023-09-25 13:53:59 +0545

    code added

---
+++ salaryeg.py
@@ -0,0 +1,6 @@
+#suppose you are a university student and you need to pay $ 4535 tution fee for the next semester. The college is giving the discount of 10% on the early payment f your tution fee. Since its a good offer you decided to make an early payment. can you find out how much money will you have to pay?
+fee = 4535
+discount_percent = 10
+discount_amt = (discount_percent /100) * fee
+discounted_fee = fee - discount_amt
+print("fee after discount",discounted_fee)
